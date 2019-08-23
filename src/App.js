1ae0b9c68936ac2fd1eaa97094b8a680c4831eb9
GuidoOffermans <guidooffermans@hotmail.com>
2019-08-23 19:54:19 +0200

    made Select component

--- src/App.js
+++ src/App.js
@@ -1,47 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Select from './components/Select/Select'
+
 class App extends Component {
-	state = {
-		data: [
-			{
-				name: 'Ivel Z3',
-				manufacturer: 'Ivasim',
-				year: 1969,
-				origin: 'Croatia'
-			},
-			{
-				name: 'Bally Astrocade',
-				manufacturer: 'Bally Consumer Products',
-				year: 1977,
-				origin: 'USA'
-			},
-			{
-				name: 'Sord M200 Smart Home Computer',
-				manufacturer: 'Sord Computer Corporation',
-				year: 1971,
-				origin: 'Japan'
-			},
-			{
-				name: 'Commodore 64',
-				manufacturer: 'Commodore',
-				year: 1982,
-				origin: 'USA'
-			}
-		]
-	};
+
+  updateSelection = (event) => {
+    event.preventDefault()
+    // console.log('update', event.target.value)
+    // this.setState({selectValue: event.target.value})
+  }
+
 	render() {
-    const { data } = this.state
+    
 		return (
 			<div className="App">
-				<select>
-          <option>{`>-- pick a model --<`}</option>
-          {data.map(dataItem => 
-            <option 
-              key={dataItem.name} 
-              value={dataItem.name}>{`${dataItem.name} (${dataItem.year})`}
-            </option>)}
-				</select>
+				<Select updateSelection={() => this.updateSelection}/>
 			</div>
 		);
 	}
