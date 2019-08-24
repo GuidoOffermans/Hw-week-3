b9dbb583a54b558e9bb7bba6d3b4493647a861d9
GuidoOffermans <guidooffermans@hotmail.com>
2019-08-24 10:46:03 +0200

    rolled back on my decision

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Select from './components/Select/Select'
-
 class App extends Component {
+  state = {
+    selectValue: '',
+		data: [
+			{
+				name: 'Ivel Z3',
+				manufacturer: 'Ivasim',
+				year: 1969,
+				origin: 'Croatia'
+			},
+			{
+				name: 'Bally Astrocade',
+				manufacturer: 'Bally Consumer Products',
+				year: 1977,
+				origin: 'USA'
+			},
+			{
+				name: 'Sord M200 Smart Home Computer',
+				manufacturer: 'Sord Computer Corporation',
+				year: 1971,
+				origin: 'Japan'
+			},
+			{
+				name: 'Commodore 64',
+				manufacturer: 'Commodore',
+				year: 1982,
+				origin: 'USA'
+			}
+		]
+  };
 
   updateSelection = (event) => {
     event.preventDefault()
     // console.log('update', event.target.value)
-    // this.setState({selectValue: event.target.value})
+    this.setState({selectValue: event.target.value})
   }
 
 	render() {
-    
+    console.log("NEW STATE:", this.state.selectValue)
+    const { data } = this.state
 		return (
 			<div className="App">
-				<Select updateSelection={() => this.updateSelection}/>
+				 <select onChange={this.updateSelection}>
+          <option>{`>-- pick a model --<`}</option>
+          {data.map(dataItem => 
+            <option 
+              key={dataItem.name} 
+              value={dataItem.name}>{`${dataItem.name} (${dataItem.year})`}
+            </option>)}
+				</select>
 			</div>
 		);
 	}
